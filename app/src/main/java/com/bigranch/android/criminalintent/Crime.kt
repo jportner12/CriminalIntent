e876550208830911863be5aebf82389ec78df389
Jacob Portner <jportne3@gustavus.edu>
2020-03-31 22:34:07 -0500

    Testing Update from Android Studio, check in Crime.kt that the class is multiple line definition instead of one.

--- app/src/main/java/com/bigranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bigranch/android/criminalintent/Crime.kt
@@ -3,6 +3,10 @@ package com.bigranch.android.criminalintent
 import java.text.DateFormat
 import java.util.*
 
-data class Crime(val id: UUID =UUID.randomUUID(), var title:String = "", var date: String = DateFormat.getDateInstance(DateFormat.FULL, Locale.ENGLISH).format(Date().time), var isSolved:Boolean = false, var requiresPolice: Boolean = false){
+data class Crime(val id: UUID =UUID.randomUUID(),
+                 var title:String = "",
+                 var date: String = DateFormat.getDateInstance(DateFormat.FULL, Locale.ENGLISH).format(Date().time),
+                 var isSolved:Boolean = false,
+                 var requiresPolice: Boolean = false){
 
 }
